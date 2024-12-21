dd172199b3ecff09706c6c250d01f3ab0e88cadd
bluemoon983 <msdrobot@naver.com>
2024-12-21 22:55:34 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:onsemiro/screens/login_screens.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // 일정 시간 후 LoginScreens로 이동
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const LoginScreens()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFFDE047), // 배경색 설정
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/Ralo.png', // 로고 이미지 경로
+              height: 100,
+            ),
+            const SizedBox(height: 20),
+            const CircularProgressIndicator(), // 로딩 인디케이터
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/login_screens.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+
+class LoginScreens extends StatelessWidget {
+  const LoginScreens({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start, // Column 내 왼쪽 정렬
+        children: [
+          const SizedBox(
+            height: 150,
+          ),
+          Row(
+            children: [
+              const SizedBox(
+                width: 100,
+              ),
+              Image.asset(
+                "assets/Ralo.png",
+                width: 200,
+                height: 200,
+                fit: BoxFit.cover,
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 35.0), // 왼쪽 여백 설정
+            child: Text(
+              "아이디",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 35.0), // 좌우 여백 설정
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: "아이디를 입력하세요", // 힌트 텍스트 추가
+                enabledBorder: UnderlineInputBorder(
+                  // 밑선 색 변경
+                  borderSide: BorderSide(color: Colors.yellow), // 원하는 색상으로 변경
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  // 포커스 시 밑선 색 변경
+                  borderSide: BorderSide(color: Colors.yellow), // 원하는 색상으로 변경
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 35.0), // 왼쪽 여백 설정
+            child: Text(
+              "비밀번호",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 35.0), // 좌우 여백 설정
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: "아이디를 입력하세요", // 힌트 텍스트 추가
+                enabledBorder: UnderlineInputBorder(
+                  // 밑선 색 변경
+                  borderSide: BorderSide(color: Colors.yellow), // 원하는 색상으로 변경
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  // 포커스 시 밑선 색 변경
+                  borderSide: BorderSide(color: Colors.yellow), // 원하는 색상으로 변경
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 32,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // 로그인 로직 추가
+                },
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(333, 20),
+                  maximumSize: Size(333, 50),
+                  foregroundColor: Colors.white,
+                  backgroundColor: Color(0xFFFDE047), // 텍스트 색상
+                  padding: EdgeInsets.symmetric(
+                      horizontal: 50, vertical: 15), // 패딩 설정
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10), // 모서리 둥글게
+                  ),
+                ),
+                child: const Text(
+                  "로그인",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 300,
+              ),
+              TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "회원가입",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
